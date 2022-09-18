3c30f480afe22295512dc01f8231e159e4fdc404
Irfan Hanif <14.irfanh@gmail.com>
2022-09-18 11:26:23 +0700

    added bagian5a code

---
+++ Kelompok_2/Lampiran/Kode/bagian5a/register.php
@@ -0,0 +1,17 @@
+<html>
+    <body>
+    <h1>Register</h1>
+    <form action="register_process.php" method="post">
+
+    <label>
+        Username: <input type="text" name="username"><br>
+    </label>
+    <label>
+        Password:<input type="password" name="password"><br>
+    </label>
+    <input type="submit" value="Register">
+
+    </form>
+    </body>
+</html>
+
