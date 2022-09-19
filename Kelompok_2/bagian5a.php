66644e42af7793d78417df2179fa90e9bced41da
Irfan Hanif <14.irfanh@gmail.com>
2022-09-19 09:51:04 +0700

    finished bagian5a and added supporting files

--- Kelompok_2/bagian5a.php
+++ Kelompok_2/bagian5a.php
@@ -1,5 +1,34 @@
 <html>
     <body>
+        <h1>Tugas Review Materi</h1>
+        <h2><em>Study Case</em> Registrasi dan Login</h2>
         
+        <p>Pada bagian ini, kami mempraktikkan <em>source code</em> pada materi Form Handling yang menggunakan $_POST, 
+        yaitu sebuah variabel superglobal yang digunakan untuk mengumpulkan form-data.</p>
+
+        <p>Dalam uji coba ini, kami membuat 4 file yang digunakan untuk melakukan proses registrasi dan login, yaitu:</p>
+        <ol>
+            <li> <a href="Lampiran/Kode/bagian5a/register.php">register.php</a></li>
+            <li> <a href="Lampiran/Kode/bagian5a/register_process.php">register_process.php</a></li>
+            <li> <a href="Lampiran/Kode/bagian5a/login.php">login.php</a></li>
+            <li> <a href="Lampiran/Kode/bagian5a/login_process.php">login_process.php</a></li>
+        </ol>
+
+        <p>Proses register, user mengisi form username dan password dan ketika user meng-<em>click</em> tombol submit
+        maka data username dan password dikirim ke file PHP <a href="Lampiran/Kode/bagian5a/register_process.php">register_process.php</a>
+        untuk diproses dengan HTTP POST. Karena dalam praktik ini hanya perlu menampilkan username dan password maka di dalam file PHP dengan <em>echo</em>.</p>
+
+        <img src="Lampiran/Foto/bagian5a/register_output.png" alt="Output dari register_process.php">
+
+        <p>Proses login, user mengisi form username dan password dan ketika user meng-<em>click</em> tombol submit
+        maka data username dan password dikirim ke file PHP <a href="Lampiran/Kode/bagian5a/login_process.php">login_process.php</a>
+        untuk diproses dengan HTTP POST. Di file PHP, data username dan password akan dicek dengan menggunakan <em><b>if</b></em>. 
+        Jika username dan password yang dicek benar, maka akan menghasilkan output:</p>
+
+        <img src="Lampiran/Foto/bagian5a/login_correct_output.png" alt="Output dari login_process.php apabila benar">
+
+        <p>Sebaliknya, jika username dan password dicek salah, maka akan menghasilkan output:</p> 
+
+        <img src="Lampiran/Foto/bagian5a/login_incorrect_output.png" alt="Output dari login_process.php apabila salah">
     </body>
 </html>
