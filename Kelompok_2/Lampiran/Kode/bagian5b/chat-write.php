fffe1d226bb461d442af3a46fd9d4d38eedf79b4
Irfan Hanif <14.irfanh@gmail.com>
2022-09-20 19:27:22 +0700

    fixed bagian5a files

--- Kelompok_2/Lampiran/Kode/bagian5b/chat-write.php
+++ Kelompok_2/Lampiran/Kode/bagian5b/chat-write.php
@@ -1,5 +1,5 @@
 <?php
 
-$message = $_GET[‘message’];
+$message = $_GET['message'];
 
-file_put_contents(‘chat.txt’, “$message\n”, FILE_APPEND);
+file_put_contents('chat.txt', "$message\n", FILE_APPEND);
