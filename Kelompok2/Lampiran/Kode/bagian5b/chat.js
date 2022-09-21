c9b3a2ae8c1cbdaf684391d5b6aa892ad46cae75
simjake02 <112856103+simjake02@users.noreply.github.com>
2022-09-21 09:39:26 +0700

    Update

--- Kelompok2/Lampiran/Kode/bagian5b/chat.js
+++ Kelompok2/Lampiran/Kode/bagian5b/chat.js
@@ -1,42 +1,32 @@
 <script>
+    
 const chat = document.querySelector(‘#chat’);
 const message = document.querySelector(‘#message’);
 const baseUrl = ‘http://localhost/chat’;
 
 function readChat() {
-fetch(`${baseUrl}/chat-read.php`)
-.then(res => res.text())
-.then(res => {
-chat.value = res;
-});
+    fetch(`${baseUrl}/chat-read.php`)
+        .then(res => res.text())
+        .then(res => {
+            chat.value = res;
+        });
 
-setTimeout(readChat, 1000);
+    setTimeout(readChat, 1000);
 };
 
 readChat();
-const button = document.querySelector(‘#greet’);
 
-button.addEventListener(‘click’
+message.addEventListener('keyup', e = {
+    if (e.keyCode === 13) {
+        fetch(`${baseUrl}/chat-write.php`, {
+            method: 'post',
+            headers: {
+                'Content-Type': 'application/x-www-form-urlencoded',
+            },
+            body: `text=${message.value}`
+        });
 
-, () => {
-
-const name = document.querySelector(‘#name’).value;
-const url = ‘http://localhost/ajax/ajax.php’;
-
-fetch(url, {
-method: ‘post’
-,
-
-headers: {
-‘Content-Type’: ‘application/x-www-form-urlencoded’
-,
-
-},
-body: `name=${name}`
-})
-.then(res => res.text())
-.then(res => {
-alert(res);
-});
+    message.value = '';
+    }
 });
 </script>
