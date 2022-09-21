b6dde54b527bc847b33db176aa6c6c4ec30c8998
simjake02 <112856103+simjake02@users.noreply.github.com>
2022-09-21 08:41:45 +0700

    update

---
+++ Kelompok_2/Lampiran/Kode/bagian5b/chat.js
@@ -0,0 +1,43 @@
+<script>
+
+const chat = document.querySelector('#chat');
+const message = document.querySelector('#message');
+const baseUrl = 'http://localhost/chat';
+
+function readChat() {
+fetch(`${baseUrl}/chat-read.php`)
+.then(res => res.text())
+.then(res => {
+chat.value = res;
+});
+
+setTimeout(readChat, 1000);
+};
+
+readChat();
+const button = document.querySelector('#greet');
+
+button.addEventListener('click'
+
+ {
+
+const name = document.querySelector('#name').value;
+const url = 'http://localhost/ajax/ajax.php';
+
+fetch(url, {
+method: 'post'
+,
+
+headers: {
+'Content-Type': 'application/x-www-form-urlencoded'
+
+
+},
+body: `name=${name}`
+})
+.then(res = res.text())
+.then(res = {
+alert(res)
+});
+});
+</script>
