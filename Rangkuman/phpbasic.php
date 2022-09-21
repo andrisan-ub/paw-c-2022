4cd1bb606b6127b34873fddfe230f8db7bfae177
Eci Emriyeni <eciemriyeni@student.ub.ac.id>
2022-09-21 19:44:32 +0700

    Create phpbasic.php

---
+++ Rangkuman/phpbasic.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://cdn.tailwindcss.com"></script>
+    <title>Kelompok 4 PAW</title>
+    <link rel="stylesheet" href="style.css">
+
+    <link href="https://cdn.jsdelivr.net/npm/daisyui@2.28.0/dist/full.css" rel="stylesheet" type="text/css" />
+    <script src="https://cdn.tailwindcss.com"></script>
+
+
+    <link href="https://cdn.jsdelivr.net/npm/daisyui@2.28.0/dist/full.css" rel="stylesheet" type="text/css" />
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat&display=swap" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2/dist/tailwind.min.css" rel="stylesheet" type="text/css" />
+    <style>
+        @font-face {
+            font-family: studly;
+            src: url('../img/font/Studly\ Regular.ttf') format('truetype');
+        }
+    </style>
+
+</head>
+
+<body style="background-color: black; background-size: cover;">
+    <div class="text-center justify-center  md:h-[20vh] flex ">
+        <h1 class="text-4xl text-white font-[studly]">PHP BASIC</h1>
+        <a href="../index.html" > <button class="text-black bg-white rounded-lg px-4 py-2  absolute float-right md:right-10">Beranda</button></a>
+    </div>
+
+    <div class="md:flex md:space-x-5">
+        <div class="card w-96 bg-base-100 shadow-xl ">
+            <div class="card-body">
+                <h2 class="card-title text-white text-xl font-[studly]">Halaman 14</h2>
+                <img src="../img/14 code.png" alt="">
+                <img src="../img/14 run.png" alt="">
+                <p class="text-white font-['Montserrat',sans-serif]">Syntax diatas merupakan syntax yang salah dan apabila dijalankan akan error karena tidak menggunakan notasi $. Fungsi notasi $ adalah mendeklarasikan sebuah variabel, jika tidak ada notasi $ maka program tidak bisa mendeteksi jika myFunction adalah sebuah variabel.</p>
+               
+            </div>
+        </div>
+        <div class="card w-96 bg-base-100 shadow-xl ">
+            <div class="card-body">
+                <h2 class="card-title text-white text-xl font-[studly]">Halaman 22</h2>
+                <img src="../img/22 code.png" alt="">
+               
+                <p class="text-white font-['Montserrat',sans-serif]">Syntax diatas adalah contoh bagaimana sebuah variabel dideklarasikan pada bahasa pemrograman PHP. Caranya adalah dengan menambahkan notasi $ di depan nama variabel seperti contoh di atas.</p>
+               
+            </div>
+        </div>
+        <div class="card w-96 bg-base-100 shadow-xl ">
+            <div class="card-body">
+                <h2 class="card-title text-white text-xl font-[studly]">Halaman 26</h2>
+                <img src="../img/26 code.png" alt="">
+               
+                <p class="text-white font-['Montserrat',sans-serif]">Di awal ditulis bahwa variabel $foo bernilai sebuah string “bar”. Lalu pada baris kedua nilai variabel $foo diganti menjadi string “world” dengan cara menambahkan notasi $ didepan, menjadi $$foo. Lalu pada baris ketiga, langkah sebelumnya diulang dengan menambahkan notasi $ lagi di depan nama variabel $$foo, dan nilainya diganti menjadi “100”. Maka, output yang ditampilkan ketika syntax diatas dijalankan adalah “100”</p>
+               
+            </div>
+        </div>
+
+
+</body>
+
+</html>
