19cd797d9c3ea8952aeee04f68fe73f2d7d05030
nabilghi55 <nabilghi55@student.ub.ac.id>
2022-09-21 20:31:03 +0700

    Update index.php

--- Rangkuman/AJAX/index.php
+++ Rangkuman/AJAX/index.php
@@ -19,11 +19,10 @@
             src: url('../img/font/Studly\ Regular.ttf') format('truetype');
         }
 
-        .tugas {}
     </style>
     <meta charset="utf-8" />
 
-    <title>Tuts+ Chat Application</title>
+    <title>Ajax Chat app</title>
     <meta name="description" content="Tuts+ Chat Application" />
     <link rel="stylesheet" href="style.css" />
 </head>
@@ -33,7 +32,7 @@ session_start();
 
 if (isset($_GET['logout'])) {
 
-    //Simple exit message
+    //exit
     $logout_message = "<div class='msgln'><span class='left-info'>User <b class='user-name-left'>" . $_SESSION['name'] . "</b> has left the chat session.</span><br></div>";
     file_put_contents("log.html", $logout_message, FILE_APPEND | LOCK_EX);
 
@@ -70,6 +69,11 @@ function loginForm()
 
 
 ?>  
+<style>
+    p{
+        font: studly;
+    }
+</style>
 <body>
 <?php
     if (!isset($_SESSION['name'])) {
@@ -96,5 +100,49 @@ function loginForm()
                 <input class="btn btn-active ml-3" name="submitmsg" type="submit" id="submitmsg" value="Send" />
             </form>
         </div>
+
+        <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+        <script type="text/javascript">
+            // jQuery Document
+            $(document).ready(function() {
+                $("#submitmsg").click(function() {
+                    var clientmsg = $("#usermsg").val();
+                    $.post("post.php", {
+                        text: clientmsg
+                    });
+                    $("#usermsg").val("");
+                    return false;
+                });
+
+                function loadLog() {
+                    var oldscrollHeight = $("#chatbox")[0].scrollHeight - 20; //Scroll height before the request
+
+                    $.ajax({
+                        url: "log.html",
+                        cache: false,
+                        success: function(html) {
+                            $("#chatbox").html(html); //Insert chat log into the #chatbox div
+
+                            //Auto-scroll           
+                            var newscrollHeight = $("#chatbox")[0].scrollHeight - 20; //Scroll height after the request
+                            if (newscrollHeight > oldscrollHeight) {
+                                $("#chatbox").animate({
+                                    scrollTop: newscrollHeight
+                                }, 'normal'); //Autoscroll to bottom of div
+                            }
+                        }
+                    });
+                }
+
+                setInterval(loadLog, 2500);
+
+                $("#exit").click(function() {
+                    var exit = confirm("Are you sure you want to end the session?");
+                    if (exit == true) {
+                        window.location = "index.php?logout=true";
+                    }
+                });
+            });
+        </script>
 </body>
 </html>
