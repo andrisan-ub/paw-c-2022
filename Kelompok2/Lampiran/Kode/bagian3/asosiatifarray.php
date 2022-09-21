3b884e8d38e2ce66cd2cfaebb64328b143ea539e
fauzanzakaria <fauzanzakaria449@gmail.com>
2022-09-21 14:26:11 +0700

    Create asosiatifarray.php

---
+++ Kelompok2/Lampiran/Kode/bagian3/asosiatifarray.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+  $siswas = [
+    'Andi' => 'Belajar', 
+    'Deni' => 'Membaca', 
+    'Budi' => 'Bermain'
+  ];
+  foreach ($siswas as $siswa => $siswa_value) { 
+    echo "Key =" . $siswa . ", Value=" . $siswa_value;
+    echo "<br>";
+  }
+?>
+
+</body>
+</html>
