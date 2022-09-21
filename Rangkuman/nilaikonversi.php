4163e447b54b08e5d68228ee7610069edeabb40b
Rizka Winal Sabrina <103319544+rizkawinalsabrina@users.noreply.github.com>
2022-09-21 19:49:32 +0700

    Create nilaikonversi.php

---
+++ Rangkuman/nilaikonversi.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Kelompok 4 PAW</title>
+    <link href="https://cdn.jsdelivr.net/npm/daisyui@2.28.0/dist/full.css" rel="stylesheet" type="text/css" />
+    <script src="https://cdn.tailwindcss.com"></script>
+    <link href="https://cdn.jsdelivr.net/npm/daisyui@2.28.0/dist/full.css" rel="stylesheet" type="text/css" />
+    <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2/dist/tailwind.min.css" rel="stylesheet" type="text/css" />
+    <style>
+        @font-face {
+            font-family: studly;
+            src: url('../img/font/Studly\ Regular.ttf') format('truetype');
+        }
+    </style>
+</head>
+<body>
+<div class="text-center justify-center  md:h-[20vh] flex ">
+        <h1 class="text-3xl text-white font-[studly]">Penerapan Materi Terhadap Konversi Nilai Mahasiswa</h1>
+        <a href="../index.html" > <button class="text-black bg-white rounded-lg px-4 py-2  absolute float-right md:right-10">Beranda</button></a>
+    </div>
+    <form method="POST" class=" " action=" ">
+            <div class="m-auto">
+                <div class="flex  items-center justify-center text-center m-auto bg-white w-96 h-60">
+                    <div class="text-center justify-center ">
+                        <label class="text-black -" for="nilai">Masukkan Nilai</label> <br>
+                        <input id="nilai" type="text" name="nilai" placeholder="Nilai" class="input input-bordered input-info w-full max-w-xs" autocomplete="off"/>
+                        
+                    </div>
+                    <div class="absolute mt-44">
+                        <button class="btn btn-outline" type="submit" name="register">Cek Indeks Nilai Anda</button>
+                    </div>
+
+                </div>
+                
+                
+            </div>
+        </form>
+</body>
+</html>
