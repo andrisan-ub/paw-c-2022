d388ddde9cb62d85db8bacb01a2ef2342bfce17d
Irfan Hanif <14.irfanh@gmail.com>
2022-09-22 13:56:46 +0700

    created php files for bagian5a

---
+++ Kelompok2/Lampiran/Kode/bagian5a/login.php
@@ -0,0 +1,15 @@
+<html>
+    <body>
+        <form action="login_process.php" method="post">
+
+        <label>
+            Username: <input type="text" name="username"><br>
+        </label>
+        <label>
+            Password: <input type="password" name="password"><br>
+        </label>
+        <input type="submit" value="Login">
+
+        </form>    
+    </body>
+</html>
---
+++ Kelompok2/Lampiran/Kode/bagian5a/register_process.php
@@ -0,0 +1,6 @@
+<?php
+
+$username = $_POST["username"];
+$password = $_POST["password"];
+
+echo ("Registering $username with password $password");
