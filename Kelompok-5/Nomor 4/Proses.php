27c0aeb3e3480637b93d972dfbfad742ce202915
alhavydz <112816854+alhavydz@users.noreply.github.com>
2022-09-27 21:47:51 +0700

    Add files via upload

---
+++ Kelompok-5/Nomor 4/Proses.php
@@ -0,0 +1,29 @@
+<?php
+include 'Index.php';
+for ($i = 0; $i < $_POST['kolom']; $i++) {
+    if (isset($_POST["nilaiMatkul$i"])) {
+        $nilai[] = $_POST["nilaiMatkul$i"];
+    }
+}
+?>
+<table style="border: 1px solid black">
+    <tr style="border: 1px solid black">
+        <th style="border: 1px solid black">Nilai Mata Kuliah</th>
+        <th style="border: 1px solid black">Nilai Mutu</th>
+        <th style="border: 1px solid black">Huruf Mutu</th>
+    </tr>
+
+    <?php if (isset($nilai)) : ?>
+        <?php for ($i = 0; $i < count($nilai); $i++) : ?>
+            <?php $num = $i + 1;?>
+            <tr style="border: 1px solid black">
+                <td style="border: 1px solid black"><?php echo "Matkul $num: $nilai[$i]" ?></td>
+                <td style="border: 1px solid black"><?php echo hasilMutu($nilai[$i])?></td>
+                <td style="border: 1px solid black"><?php echo hurufMutu($nilai[$i])?></td>
+            </tr>
+        <?php endfor; ?>
+    <?php endif; ?>
+
+</table>
+<br>
+<button><a href="/Nomor 4/Nomor4.html" style="text-decoration: none;">Back</a></button>
