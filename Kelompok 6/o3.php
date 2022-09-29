87f31033c017c08cc27f54ada5d94e680eb31043
Anas4rif <anas4rif1220@student.ub.ac.id>
2022-09-29 10:45:33 +0700

    Create o3.php

---
+++ Kelompok 6/o3.php
@@ -0,0 +1,22 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/daisyui@2.28.0/dist/full.css" rel="stylesheet" type="text/css" />
+    <script src="https://cdn.tailwindcss.com"></script>
+    <title>Document</title>
+</head>
+<body style="font-family:'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif; color: white;" class="mx-5 my-10 text-center">
+    <?php
+        $tumbuhan = ['mawar', 'anggrek'];
+        $tumbuhan[] = 'bangkai';
+
+        for ($i = 0; $i <= count($tumbuhan) - 1; $i++){
+            if ($tumbuhan[$i] == 'melati' || $tumbuhan[$i] == 'anggrek' || $tumbuhan[$i] == 'bangkai'){
+                echo "Nama tumbuhan sudah benar <br/>";
+            } else {echo "Nama tumbuhan adalah $tumbuhan[$i] <br>";}
+        }
+    ?>    
+</body>
+</html>
