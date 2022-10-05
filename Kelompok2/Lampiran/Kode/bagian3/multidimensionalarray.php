33fa5567d830b32f961e8f68a1406e46af12d79d
fauzanzakaria <fauzanzakaria449@gmail.com>
2022-10-05 21:13:51 +0700

    Update multidimensionalarray.php

--- Kelompok2/Lampiran/Kode/bagian3/multidimensionalarray.php
+++ Kelompok2/Lampiran/Kode/bagian3/multidimensionalarray.php
@@ -1,10 +1,15 @@
 <?php
 
-$array = [
+$numbers = [
     ['satu', 1],
     ['dua', 2],
     ['tiga', 3]
 ];
-echo($array);
+
+foreach ($numbers as $number) {
+    foreach ($number as $value) {
+        echo $value . '<br>';
+    }
+}
 
 ?>
